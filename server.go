85e3b0477ff32c1ebd45824a762b539c1e2e53a3
Tomas Torres <tomastorres003@hotmail.com>
2023-01-26 21:15:59 -0300

    Added DELETE and PUT methods

--- server.go
+++ server.go
@@ -17,6 +17,11 @@ type task struct {
 	Content string `json:"Content"`
 }
 
+type updateTask struct {
+	Name    string `json:"Name"`
+	Content string `json:"Content"`
+}
+
 type allTasks []task
 
 var tasks = allTasks{
@@ -28,16 +33,16 @@ var tasks = allTasks{
 }
 
 func indexRoute(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
+	if r.Method != http.MethodGet {
 		w.WriteHeader(405) // Return 405 Method Not Allowed.
 		return
 	}
 	fmt.Fprintf(w, "Welcome to my API")
 }
 
-func getTasks(w http.ResponseWriter, r *http.Request) {
+func getTasksRoute(w http.ResponseWriter, r *http.Request) {
 
-	if r.Method != http.MethodPost {
+	if r.Method != http.MethodGet {
 		w.WriteHeader(405) // Return 405 Method Not Allowed.
 		return
 	}
@@ -46,9 +51,9 @@ func getTasks(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(tasks)
 }
 
-func getTask(w http.ResponseWriter, r *http.Request) {
+func getTaskByIdRoute(w http.ResponseWriter, r *http.Request) {
 
-	if r.Method != http.MethodPost {
+	if r.Method != http.MethodGet {
 		w.WriteHeader(405) // Return 405 Method Not Allowed.
 		return
 	}
@@ -71,10 +76,9 @@ func getTask(w http.ResponseWriter, r *http.Request) {
 
 	log.Printf("ID doesn't exist")
 	w.WriteHeader(400) // Return 400 Bad Request.
-
 }
 
-func createTask(w http.ResponseWriter, r *http.Request) {
+func createTaskRoute(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method != http.MethodPost {
 		w.WriteHeader(405) // Return 405 Method Not Allowed.
@@ -105,13 +109,87 @@ func createTask(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(newTask)
 }
 
+func updateTaskRoute(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodPut {
+		w.WriteHeader(405)
+		return
+	}
+
+	var newTask updateTask
+	reqBody, err := io.ReadAll(r.Body)
+
+	if err != nil {
+		log.Printf("Body read error, %v", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	vars := mux.Vars(r)
+	taskID, err := strconv.Atoi(vars["id"])
+
+	if err != nil {
+		log.Printf("ID read error, %v", err)
+		w.WriteHeader(400) // Return 400 Bad Request.
+		return
+	}
+
+	if err = json.Unmarshal(reqBody, &newTask); err != nil {
+		log.Printf("Body parse error, %v", err)
+		w.WriteHeader(400) // Return 400 Bad Request.
+		return
+	}
+
+	for i, task := range tasks {
+		if task.ID == taskID {
+			tasks[i].Name = newTask.Name
+			tasks[i].Content = newTask.Content
+			w.WriteHeader(200)
+			return
+		}
+	}
+
+	log.Printf("ID doesn't exist")
+	w.WriteHeader(400)
+
+}
+
+func deleteTask(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodDelete {
+		w.WriteHeader(405)
+		return
+	}
+
+	vars := mux.Vars(r)
+	taskID, err := strconv.Atoi(vars["id"])
+
+	if err != nil {
+		log.Printf("ID read error, %v", err)
+		w.WriteHeader(400) // Return 400 Bad Request.
+		return
+	}
+
+	for i, task := range tasks {
+		if task.ID == taskID {
+			tasks = append(tasks[:i], tasks[i+1:]...)
+			return
+		}
+	}
+
+	log.Printf("ID doesn't exist")
+	w.WriteHeader(400) // Return 400 Bad Request.
+}
+
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 
 	router.HandleFunc("/", indexRoute)
-	router.HandleFunc("/tasks", getTasks).Methods("GET")
-	router.HandleFunc("/tasks", createTask).Methods("POST")
-	router.HandleFunc("/tasks/{id}", getTask).Methods("GET")
+	router.HandleFunc("/tasks", getTasksRoute).Methods("GET")
+	router.HandleFunc("/tasks", createTaskRoute).Methods("POST")
+	router.HandleFunc("/tasks/{id}", getTaskByIdRoute).Methods("GET")
+	router.HandleFunc("/tasks/{id}", updateTaskRoute).Methods("PUT")
+	router.HandleFunc("/tasks/{id}", deleteTask).Methods("DELETE")
 
 	if err := http.ListenAndServe(":8080", router); err != nil {
 		log.Fatal(err)
